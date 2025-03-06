f4f24997c3408f41563c6355c4b80841c5d73b7d
riyadhbd2 <riyadhbd2@gmail.com>
2025-03-06 06:27:23 +0100

    nabvar and header corrected

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,26 +1,28 @@
-import React from 'react'
+import React from 'react';
 import { assets } from '../assets/assets';
 
 const Navbar = () => {
   return (
-    <div className='px-5 flex items-center justify-between mt-3'>
-      <img className='w-36' src={assets.logo} alt="" />
-      <ul className='flex gap-5 text-[#49557e] text-md cursor-pointer'>
+    <div className='flex items-center justify-between mt-3'>
+      <img className='w-28 sm:w-36' src={assets.logo} alt='Logo' />
+      <ul className='hidden md:flex gap-4 sm:gap-6 text-[#49557e] text-sm sm:text-md cursor-pointer'>
         <li>Home</li>
         <li>Menu</li>
         <li>Mobile App</li>
         <li>Contact Us</li>
       </ul>
-      <div className='flex items-center gap-10'>
-        <img src={assets.search_icon} alt="" />
+      <div className='flex items-center gap-6 sm:gap-10'>
+        <img className='w-5 sm:w-6' src={assets.search_icon} alt='Search' />
         <div className='relative'>
-          <img src={assets.basket_icon} alt="" />
-          <div className='absolute min-w-3 min-h-3 bg-orange-400 border rounded-full top-[-8px] right-[-8px]'></div>
+          <img className='w-5 sm:w-6' src={assets.basket_icon} alt='Basket' />
+          <div className='absolute min-w-3 min-h-3 bg-orange-400 border rounded-full top-[-4px] sm:top-[-8px] right-[-4px] sm:right-[-8px]'></div>
         </div>
-        <button className='text-[#49557e] border  border-orange-400 px-5 py-1 rounded-full hover:bg-[#fff4f2] hover:scale-105 transition-all duration-700'>Sign in</button>
+        <button className='text-[#49557e] border border-orange-400 px-4 sm:px-5 py-1 rounded-full hover:bg-[#fff4f2] hover:scale-105 transition-all duration-700'>
+          Sign in
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
