9dcf8e83d09f32af233493d458058a774472c1bb
riyadhbd2 <riyadhbd2@gmail.com>
2025-03-06 21:53:21 +0100

    menu filter added

--- client/src/components/FoodDisplay.jsx
+++ client/src/components/FoodDisplay.jsx
@@ -10,7 +10,10 @@ const FoodDisplay = ({ category }) => {
       <h2 className='text-2xl font-semibold text-center sm:text-left'>Top dishes near you</h2>
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-5 gap-5'>
         {food_list.map((item, index) => {
-          return <FoodItem item={item} key={index} />;
+            if (category === 'All' || category === item.category) {
+                return <FoodItem item={item} key={index} />;
+            }
+          
         })}
       </div>
     </div>
