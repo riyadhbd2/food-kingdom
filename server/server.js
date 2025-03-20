4408687a9a20af0eb804987eb2604db2450dca5f
Easir Arafat <riyadhbd2@Easirs-MacBook-Air.local>
2025-03-20 14:22:21 +0100

    cart controller dunctions added

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
+import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import connectDB from './config/db.js';
 import foodRouter from './routes/foodRoute.js';
 import userRouter from './routes/userRoute.js';
-import 'dotenv/config';
+
+import cartRouter from './routes/cartRoute.js';
 
 const app = express();
 
@@ -20,6 +22,7 @@ connectDB();
 app.use('/api/food', foodRouter);
 app.use("/images", express.static('uploads'));
 app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
 
 
 app.get('/', (req, res)=>{
