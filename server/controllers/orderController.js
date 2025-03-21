0010bb5dda044f7b949b951596f8da1c774cef13
Easir Arafat <riyadhbd2@Easirs-MacBook-Air.local>
2025-03-21 18:00:41 +0100

    get all orders for admin added

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,60 +1,100 @@
-import Stripe from 'stripe';
-import orderModel from '../models/orderModel.js';
-import userModel from '../models/userModel.js';
+import Stripe from "stripe";
+import orderModel from "../models/orderModel.js";
+import userModel from "../models/userModel.js";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // placing user order from frontend
 const placeOrder = async (req, res) => {
-    const frontend_url = "http://localhost:5174"; // Your frontend URL
-    try {
-        const newOrder = new orderModel({
-            userId: req.body.userId,
-            items: req.body.items,
-            amount: req.body.amount,
-            address: req.body.address
-        });
+  const frontend_url = "http://localhost:5174"; // Your frontend URL
+  try {
+    const newOrder = new orderModel({
+      userId: req.body.userId,
+      items: req.body.items,
+      amount: req.body.amount,
+      address: req.body.address,
+    });
 
-        await newOrder.save();
-        await userModel.findByIdAndUpdate(req.body.userId, { cartData: {} });
+    await newOrder.save();
+    await userModel.findByIdAndUpdate(req.body.userId, { cartData: {} });
 
-        // for stripe payment
-        const line_items = req.body.items.map((item) => ({
-            price_data: {
-                currency: "eur", // Changed to EUR if you're processing payments in Euros
-                product_data: {
-                    name: item.name
-                },
-                unit_amount: item.price * 100 // Amount in cents (no need for 100*80 unless specifically required)
-            },
-            quantity: item.quantity
-        }));
+    // for stripe payment
+    const line_items = req.body.items.map((item) => ({
+      price_data: {
+        currency: "eur", // Changed to EUR if you're processing payments in Euros
+        product_data: {
+          name: item.name,
+        },
+        unit_amount: item.price * 100, // Amount in cents (no need for 100*80 unless specifically required)
+      },
+      quantity: item.quantity,
+    }));
 
-        // Adding Delivery Charge
-        line_items.push({
-            price_data: {
-                currency: "eur", // Changed to EUR if you're processing payments in Euros
-                product_data: {
-                    name: "Delivery Charge"
-                },
-                unit_amount: 200 // Delivery charge in cents (2 EUR)
-            },
-            quantity: 1
-        });
+    // Adding Delivery Charge
+    line_items.push({
+      price_data: {
+        currency: "eur", // Changed to EUR if you're processing payments in Euros
+        product_data: {
+          name: "Delivery Charge",
+        },
+        unit_amount: 200, // Delivery charge in cents (2 EUR)
+      },
+      quantity: 1,
+    });
 
-        // Create session for Stripe Checkout
-        const session = await stripe.checkout.sessions.create({
-            line_items: line_items,
-            mode: 'payment',
-            success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
-            cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`
-        });
+    // Create session for Stripe Checkout
+    const session = await stripe.checkout.sessions.create({
+      line_items: line_items,
+      mode: "payment",
+      success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
+      cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
+    });
 
-        res.json({ success: true, session_url: session.url });
-    } catch (error) {
-        console.error(error);
-        res.json({ success: false, message: "Error" });
-    }
+    res.json({ success: true, session_url: session.url });
+  } catch (error) {
+    console.error(error);
+    res.json({ success: false, message: "Error" });
+  }
 };
 
-export { placeOrder };
+// using webhook is the best practice but now using temporary
+// verify order
+const verifyOrder = async (req, res) => {
+  const { orderId, success } = req.body;
+  try {
+    if (success == "true") {
+      await orderModel.findByIdAndUpdate(orderId, { payment: true });
+      res.json({ success: true, message: "Paid" });
+    } else {
+      await orderModel.findByIdAndDelete(orderId);
+      res.json({ success: false, message: "Not Paid" });
+    }
+  } catch (error) {
+    console.error(error);
+    req.json({ success: false, message: "Error" });
+  }
+};
+
+// users orders for frontend
+const usersOrder = async (req, res) => {
+  try {
+    const orders = await orderModel.find({ userId: req.body.userId });
+    res.json({ success: true, data: orders });
+  } catch (error) {
+    console.error(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+// listing orders of Admin pannel
+const listOrders = async (req, res) => {
+  try {
+    const orders = await orderModel.find({});
+    res.json({ success: true, data: orders });
+  } catch (error) {
+    console.error(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+export { listOrders, placeOrder, usersOrder, verifyOrder };
--- server/routes/orderRoute.js
+++ server/routes/orderRoute.js
@@ -1,9 +1,12 @@
 import express from 'express'
-import { placeOrder } from '../controllers/orderController.js';
+import { listOrders, placeOrder, usersOrder, verifyOrder } from '../controllers/orderController.js';
 import authMiddleware from '../middleware/auth.js';
 
 const orderRoute = express.Router();
 
 orderRoute.post('/place', authMiddleware, placeOrder);
+orderRoute.post('/verify', verifyOrder);
+orderRoute.post('/userorders', authMiddleware, usersOrder);
+orderRoute.get('/list',listOrders );
 
 export default orderRoute;
