6494c3dab0fbd32c9d4bee0ffe8599f0721cb1ba
Easir Arafat <riyadhbd2@Easirs-MacBook-Air.local>
2025-03-22 22:33:13 +0100

    food route folder corrected

--- server/routes/foodRoute.js
+++ server/routes/foodRoute.js
@@ -7,7 +7,7 @@ import fs from 'fs';
 const foodRouter = express.Router();
 
 // Ensure 'uploads' folder exists
-const uploadDir = "/tmp/uploads";
+const uploadDir = "/uploads";
 
 if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir, { recursive: true });
